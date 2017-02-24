3d3f5d75b3910b401b69e8da491bd5ebeceac965
Felype Gustavo <felypeweb@gmail.com>
2017-02-24 12:50:04 -0300

    FInalizando o curso 1 avançado

--- Desenvolvedor-Javascript/Curso-Avancado-JS-es6/javascript-avancado-i-aula1/aluraframe/client/js/app/controllers/NegociacaoController.js
+++ Desenvolvedor-Javascript/Curso-Avancado-JS-es6/javascript-avancado-i-aula1/aluraframe/client/js/app/controllers/NegociacaoController.js
@@ -7,10 +7,16 @@ class NegociacaoController {
         this._inputData = $("#data");
         this._inputQuantidade = $("#quantidade");
         this._inputValor = $("#valor");
+        
         this._listaNegociacaoes = new ListaNegociacaoes();
+        
         this._NegociacaoView = new NegociacaoView( $("#NegociacoesView"));
-
         this._NegociacaoView.update(this._listaNegociacaoes);
+        
+        this._mensagem = new Mensagem();
+        this._mensagemView = new MensagemView($('#mensagemview'));
+        this._mensagemView.update(this._mensagem);
+        
     }
 
     adiciona(event)
@@ -19,7 +25,8 @@ class NegociacaoController {
 
         this._listaNegociacaoes.adiciona(this._criaNegociacao());
         this._limpaFormulario();
-
+        this._mensagem.texto = 'Negociação Cadastrada com Sucesso';
+        this._mensagemView.update(this._mensagem);
         this._NegociacaoView.update(this._listaNegociacaoes);
         //console.log(this._listaNegociacaoes.negociaoes);
 
--- Desenvolvedor-Javascript/Curso-Avancado-JS-es6/javascript-avancado-i-aula1/aluraframe/client/js/app/views/NegociacaoView.js
+++ Desenvolvedor-Javascript/Curso-Avancado-JS-es6/javascript-avancado-i-aula1/aluraframe/client/js/app/views/NegociacaoView.js
@@ -1,10 +1,11 @@
-class NegociacaoView {
+class NegociacaoView extends View {
     
-    constructor(elemento){
-        this._elemento = elemento;
-    }
+ constructor(elemento){
+            super(elemento);
+        }
+
     
-    _template(model){
+    template(model){
 
         return ` 
         <table class="table table-hover table-bordered">
@@ -42,27 +43,4 @@ class NegociacaoView {
 
 }
 
-update(model){
-    this._elemento.innerHTML = this._template(model);
 }
-
-}
-
-/*
-  <table class="table table-hover table-bordered">
-        <thead>
-            <tr>
-                <th>DATA</th>
-                <th>QUANTIDADE</th>
-                <th>VALOR</th>
-                <th>VOLUME</th>
-            </tr>
-        </thead>
-        
-        <tbody>
-        </tbody>
-        
-        <tfoot>
-        </tfoot>
-    </table>
-*/
